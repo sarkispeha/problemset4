78ae2aac8b82254c2efc044874436819283c5896
Sarkis Peha <sarkispeha@gmail.com>
2014-10-13 17:34:29 -0600

    going down the rabbit hole

---
+++ addNumbers.js
@@ -0,0 +1,21 @@
+//true if it is a number
+var isNumber = function(num){
+	return !isNan(parseFloat(num)) && isFinite(s);
+}
+
+var addNumbers = function(str) {
+	var addArray = [];
+	var currentGroup = '';
+
+	for(var i = 0; i < str.length; i++) {
+		var currentIndex = str.charAt(i)
+		if(!isNumber(currentIndex)) {
+			currentGroup = '';
+		}
+		else{}
+	}//end of for loop
+
+
+
+
+}//end of addNumbers
